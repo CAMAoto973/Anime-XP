d9be2f00f77dd1d563cdf6645713fbac81f3a082
CAMAoto973 <63408092+CAMAoto973@users.noreply.github.com>
2020-06-03 14:02:35 +0200

    Update main.js

--- .gitignore/main.js
+++ .gitignore/main.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js'); //Ce que le bot à besoin /
 const client = new Discord.Client(); //Que votre Bot est un nouvel utilisateur
 var prefix = "*"; //Prefix de votre Bot ( *play www.youtube.com/ )
-client.login(NzE3NTI0OTc3NTQ4NTI1NTc5.XteLIw.sW3VGkofgTaVCDontmwwvUZlMJQ); //Token (Série de chiffre) propre a chaque Bot
+client.login(NzE3NTI0OTc3NTQ4NTI1NTc5.XteQwA.H5l3R_g7dOdWOYB2_TYMFhMZHc8); //Token (Série de chiffre) propre a chaque Bot
 client.on("ready", () => { //Signifie que le bot à bien démarré console.log("Je suis prêt !"); //Lorsque que le bot est lancé observer la console Visual Studio client.user.setGame("s'Update seul"); //Voir le Jeu sur le Discord
 });
